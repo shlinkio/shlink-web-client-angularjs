5013602ab58e5c85ed746e49f879b2bcbeab00e1
Alejandro Celaya <alejandro@alejandrocelaya.com>
2016-08-26 23:59:28 +0200

    Defined base style

--- app/js/app.js
+++ app/js/app.js
@@ -3,5 +3,15 @@
 angular
     .module('shlink', ['ui.router'])
     .config(['$stateProvider', function ($stateProvider) {
-
+        $stateProvider
+            .state('main', {
+                url: '/',
+                controller : 'MainCtrl',
+                templateUrl: '/templates/main.html'
+            });
+    }])
+    .run(['$rootScope', function ($rootScope) {
+        $rootScope.$on('$stateChangeSuccess', function () {
+            $('html, body').scrollTop(0);
+        });
     }]);

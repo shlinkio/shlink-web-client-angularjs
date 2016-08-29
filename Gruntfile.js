e68599bc5d68e547766e3cb42487cc54fc30fd20
Alejandro Celaya <alejandro@alejandrocelaya.com>
2016-08-29 16:53:05 +0200

    Added unglifyjs and concat tasks to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,29 @@ module.exports = function (grunt) {
 
     cssFilesTemplate[cssFile] = 'app/sass/main.scss';
     jsFilesTemplate[jsFile] = [
-        'bower_components/angular'
+        'app/bower_components/jquery/dist/jquery.min.js',
+        'app/bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'app/bower_components/bootstrap-3-datepicker/dist/js/bootstrap-datepicker.min.js',
+        'app/bower_components/moment/min/moment.min.js',
+        'app/bower_components/angular/angular.min.js',
+        'app/bower_components/angular-ui-router/release/angular-ui-router.min.js',
+        'app/bower_components/angular-local-storage/dist/angular-local-storage.min.js',
+        'app/bower_components/angular-moment/angular-moment.min.js',
+        'app/node_modules/chart.js/dist/Chart.min.js',
+        'app/node_modules/angular-chart.js/dist/angular-chart.min.js',
+        'app/js/app.js',
+        'app/js/controllers/CreateServerCtrl.js',
+        'app/js/services/ServerService.js',
+        'app/js/services/UuidGenerator.js',
+        'app/js/directives/serversMenu.js',
+        'app/js/services/ApiService.js',
+        'app/js/controllers/ListShortUrlsCtrl.js',
+        'app/js/filters/rangeFilter.js',
+        'app/js/controllers/DeleteServerCtrl.js',
+        'app/js/directives/pagination.js',
+        'app/js/controllers/VisitsCtrl.js',
+        'app/js/services/StatsProcessor.js',
+        'app/js/controllers/CreateShortUrlCtrl.js'
     ];
 
     // Project configuration.
@@ -17,33 +39,6 @@ module.exports = function (grunt) {
         // Load configuration
         pkg: grunt.file.readJSON('package.json'),
 
-        // // Minify app JS files into one file dropping console object
-        // uglify: {
-        //     options: {
-        //         compress: {
-        //             drop_console: true
-        //         }
-        //     },
-        //     main: {
-        //         files: jsFilesTemplate
-        //     }
-        // },
-        //
-        // // Concat created CSS file and vendor CSS libraries
-        // concat: {
-        //     options: {
-        //         separator: ';\n'
-        //     },
-        //     main: {
-        //         src: [
-        //             'public/css/font-awesome.min.css',
-        //             'public/css/bootstrap.min.css',
-        //             cssFile
-        //         ],
-        //         dest: cssFile
-        //     }
-        // },
-        //
         // processhtml: {
         //     options: {
         //         // Task-specific options go here.
@@ -56,7 +51,6 @@ module.exports = function (grunt) {
         //     }
         // },
 
-        // Inspect javascript code
         jshint: {
             dashboard: {
                 src: [
@@ -73,7 +67,6 @@ module.exports = function (grunt) {
             tasks: ['sass']
         },
 
-        // Compile SASS files
         sass: {
             minified: {
                 options: {
@@ -83,6 +76,32 @@ module.exports = function (grunt) {
                 },
                 files: cssFilesTemplate
             }
+        },
+
+        concat: {
+            options: {
+                separator: ';\n'
+            },
+            main: {
+                src: [
+                    'app/bower_components/bootstrap/dist/css/bootstrap.min.css',
+                    'app/bower_components/fontawesome/css/font-awesome.min.css',
+                    'app/bower_components/bootstrap-3-datepicker/dist/css/bootstrap-datepicker3.min.css',
+                    cssFile
+                ],
+                dest: cssFile
+            }
+        },
+
+        uglify: {
+            options: {
+                compress: {
+                    drop_console: true
+                }
+            },
+            main: {
+                files: jsFilesTemplate
+            }
         }
 
     });
@@ -91,11 +110,11 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     // grunt.loadNpmTasks('grunt-processhtml');
 
     // Default task.
-    grunt.registerTask('default', ['sass'/*, 'uglify', 'concat', 'processhtml'**/]);
+    grunt.registerTask('default', ['sass', 'concat', 'uglify'/*, 'processhtml'**/]);
 
 };
